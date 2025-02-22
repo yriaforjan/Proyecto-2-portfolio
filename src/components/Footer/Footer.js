309c01b935c1ecea87bbd5171f30d4e4e5ae0560
yriaforjan <yriaforjan.oliveira@gmail.com>
2025-02-22 12:47:27 +0100

    octavo commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => `
 <h3>Contact</h3>
 <div>
     ${Button("/icons/github.png", "GitHub", "https://github.com/yriaforjan")}
-    ${Button("/icons/linkedin.png", "LinkedIn", "https://www.linkedin.com/in/yria-forj%C3%A1n-oliveira/")}
+    ${Button("/icons/linkedin.png", "LinkedIn", "https://www.linkedin.com/in/yria-forjan-oliveira/")}
     ${Button("/icons/email.png", "Email", "mailto:yriaforjan.oliveira@gmail.com")}
     ${Button("/icons/telegram.png", "Telegram", "https://t.me/yriaforjan")}
 </div>
