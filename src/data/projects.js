8386d854e6f4ccd182c3a4627cbd15e7fdebdd0d
Yria Forján Oliveira <yriaforjan.oliveira@gmail.com>
2025-04-29 22:10:57 +0200

    décimo commit - update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,14 @@
 export const projects = [
   {
-    title: "PixHive",
+    title: "My Digital Resume",
+    description: "SPA built with Vite, dynamic content generated with JavaScript, and pure CSS design.",
+    image: "/images/cv.png",
+    tech: ["HTML5", "CSS3", "JavaScript", "Vite", "EmailJS API"],
+    github: "https://github.com/yriaforjan/Proyecto-4-cv.git",
+    link: "https://yriaforjan.vercel.app/"
+  },
+  {
+    title: "PixHive App",
     description: "Asynchronous web project built with Vite and the Unsplash API to display high-quality images.",
     image: "/images/pixhive.png",
     tech: ["HTML5", "CSS3", "JavaScript", "Vite", "Unsplash API"],
@@ -9,27 +17,32 @@ export const projects = [
   },
   {
     title: "Dynamic Gallery",
-    description: "This is a dynamic gallery project using JavaScript",
-    image:
-      "/images/galeriadinamica.png",
+    description: "A dynamic gallery replicating the PcComponentes webpage layout (non-functional).",
+    image: "/images/galeriadinamica.png",
     tech: ["HTML5", "CSS3", "JavaScript"],
     github: "https://github.com/yriaforjan/practica_DOM.git",
     link: "https://practica-domgaleria-dinamica.vercel.app/",
   },
   {
+    title: "Library Management App",
+    description: "Web app to manage books: registration, search, and XML data import/export. Designed with Bootstrap.",
+    image: "/images/biblioteca.png",
+    tech: ["HTML5", "Bootstrap", "JavaScript", "XML"],
+    github: "https://github.com/yriaforjan/registro-biblioteca.git",
+    link: "https://registro-biblioteca.vercel.app/",
+  },
+  {
     title: "Small Business Website",
-    description: "This is my first complete project built with HTML5 and CSS3",
-    image:
-      "/images/bisutere.png",
+    description: "My first complete project built with HTML5 and CSS3",
+    image: "/images/bisutere.png",
     tech: ["HTML5", "CSS3"],
     github: "https://github.com/yriaforjan/Proyecto-1.git",
     link: "https://www.bisutere.com/",
   },
   {
     title: "Landing Page Prototype",
-    description: "This is my first mini-project using CSS Flexbox",
-    image:
-      "/images/jvcasesores.png",
+    description: "My first mini-project using CSS Flexbox",
+    image: "/images/jvcasesores.png",
     tech: ["HTML5", "CSS3"],
     github: "https://github.com/yriaforjan/entrega_flexbox.git",
     link: "https://jvc-asesores.vercel.app/",
